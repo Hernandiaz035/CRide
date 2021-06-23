4c4296b0178ac7ad0dffead8f559e564ffc7c774
Hernan Diaz <hernandiaz035@gmail.com>
2021-06-23 12:15:26 +1000

    Install Users App.

---
+++ cride/users/apps.py
@@ -0,0 +1,11 @@
+"""Users App."""
+
+# Django
+from django.apps import AppConfig
+
+
+class UsersAppConfig(AppConfig):
+    """Users Appconfig"""
+
+    name = 'cride.users'
+    verbose_name = 'Users'
