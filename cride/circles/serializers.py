41dc3ad33f25e240f762ab2866e67225c62d9ddb
Hernan Diaz <hernandiaz035@gmail.com>
2021-07-07 10:22:35 +1000

    Validate Unique existance before creation.

--- cride/circles/serializers.py
+++ cride/circles/serializers.py
@@ -2,6 +2,7 @@
 
 # Django REST framework
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 # Models
 from cride.circles.models import Circle
@@ -21,8 +22,12 @@ class CircleSerializer(serializers.Serializer):
 class CreateCirlceSerializer(serializers.Serializer):
     """Create Circle serializar."""
     name = serializers.CharField(max_length=140)
-    slug_name = serializers.SlugField(max_length=40)
-
+    slug_name = serializers.SlugField(
+        max_length=40,
+        validators=[
+            UniqueValidator(queryset=Circle.objects.all())
+        ]
+    )
     about = serializers.CharField(
         max_length=255,
         required=False
