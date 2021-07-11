51e462cfcc833c9d45b990e703e11fd864473202
Hernan Diaz <hernandiaz035@gmail.com>
2021-07-11 20:08:49 +1000

    Add signup endpoint.

--- cride/users/serializers/users.py
+++ cride/users/serializers/users.py
@@ -1,14 +1,16 @@
 """Circle serilizers."""
 
 # Django
-from django.contrib.auth import authenticate
+from django.contrib.auth import password_validation, authenticate
+from django.core.validators import RegexValidator
 
 # Django REST framework
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
+from rest_framework.validators import UniqueValidator
 
 # Models
-from cride.users.models import User
+from cride.users.models import (User, Profile)
 
 class UserModelSerializer(serializers.ModelSerializer):
     """User model serializer."""
@@ -24,8 +26,9 @@ class UserModelSerializer(serializers.ModelSerializer):
             'phone_number',
         )
 
+
 class UserLoginSerializer(serializers.Serializer):
-    """Handle the login request data"""
+    """Handle the login request data."""
     email = serializers.EmailField()
     password = serializers.CharField(min_length=8, max_length=64)
 
@@ -42,3 +45,49 @@ class UserLoginSerializer(serializers.Serializer):
         token, created = Token.objects.get_or_create(user=self.context['user'])
         return self.context['user'], token.key
 
+
+class UserSignupSerializer(serializers.Serializer):
+    """User signup serializer.
+
+    Handle sign up data validation and user/profile creation.
+    """
+    email = serializers.EmailField(
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    username = serializers.CharField(
+        min_length=4,
+        max_length=20,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+
+    # Phone number
+    phone_regex = RegexValidator(
+        regex=r'\+?1?\d{9,15}$',
+        message="Phone number must be entered in the format +99999999999. Up to 15 digits allowed."
+    )
+    phone_number = serializers.CharField(max_length=17,validators=[phone_regex])
+
+    # Password
+    password = serializers.CharField(min_length=8, max_length=64)
+    password_confirmation = serializers.CharField(min_length=8, max_length=64)
+
+    # Name
+    first_name = serializers.CharField(min_length=2, max_length=30)
+    last_name = serializers.CharField(min_length=2, max_length=30)
+
+    def validate(self, data):
+        """Verify passwords match."""
+        password = data['password']
+        password_confirmation = data['password_confirmation']
+        if password != password_confirmation:
+            raise serializers.ValidationError('Password confirmation does not match!.')
+        password_validation.validate_password(password)
+        return data
+
+    def create(self, data):
+        """Handle user and profile creation."""
+        data.pop('password_confirmation')
+        user = User.objects.create_user(**data)
+        profile = Profile.objects.create(user=user)
+
+        return user
