e92f3262b8d1438728c921158df1fe38552ddd16
Hernan Diaz <hernandiaz035@gmail.com>
2021-07-12 00:31:57 +1000

    Add confirmation email sending with the JWT.

--- cride/users/serializers/users.py
+++ cride/users/serializers/users.py
@@ -1,8 +1,12 @@
 """Circle serilizers."""
 
 # Django
+from django.conf import settings
 from django.contrib.auth import password_validation, authenticate
 from django.core.validators import RegexValidator
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.utils import timezone
 
 # Django REST framework
 from rest_framework import serializers
@@ -11,6 +15,10 @@ from rest_framework.validators import UniqueValidator
 
 # Models
 from cride.users.models import (User, Profile)
+
+# Utilities
+import jwt
+from datetime import time, timedelta
 
 class UserModelSerializer(serializers.ModelSerializer):
     """User model serializer."""
@@ -91,5 +99,29 @@ class UserSignupSerializer(serializers.Serializer):
         data.pop('password_confirmation')
         user = User.objects.create_user(**data, is_verified=False)
         profile = Profile.objects.create(user=user)
-
+        self.send_confirmation_email(user)
         return user
+
+    def send_confirmation_email(self, user):
+        """Send account verification link to given user."""
+        verification_token = self.gen_verification_token(user)
+
+        subject = "Welcome @{}! Verify your account to start using Comparte Ride.".format(user.username)
+        from_email = 'Comparte Ride <noreply@comparteride.com>'
+        content = render_to_string(
+            'emails/users/account_verification.html',
+            {'token': verification_token, 'user': user}
+        )
+        msg = EmailMultiAlternatives(subject, content, from_email, [user.email])
+        msg.attach_alternative(content, "text/html")
+        msg.send()
+
+    def gen_verification_token(self, user):
+        """Create the JWT token that the user can use to verify their account."""
+        exp_date = timezone.now() + timedelta(days=3)
+        payload = {
+            'username': user.username,
+            'exp': int(exp_date.timestamp()),
+            'type': 'email_confirmation'
+        }
+        return jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
