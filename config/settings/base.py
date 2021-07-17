9c16ff950ae359c950ec0fe0ea99c9bb9b8b77f1
Hernan Diaz <hernandiaz035@gmail.com>
2021-07-17 11:40:55 +1000

    Set AuthToken as the only defaul AUTH_CLASS.

--- config/settings/base.py
+++ config/settings/base.py
@@ -167,7 +167,6 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.BrowsableAPIRenderer',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authtoken'
     ],
 }
