8b5b3cf53d672802d7f23324a7da49a6fac6164d
Hernan Diaz <hernandiaz035@gmail.com>
2021-07-21 12:19:45 +1000

    Add action 'invitations' to members endpoint.
    Return a list with 2 attributes, invited_members Memberships and
    unused codes Invitations.

--- cride/circles/views/memberships.py
+++ cride/circles/views/memberships.py
@@ -1,8 +1,10 @@
 """Circle's memberships views."""
 
 # Django REST Framework
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, viewsets, status
 from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 # Permissions
 from rest_framework.permissions import IsAuthenticated
@@ -12,7 +14,7 @@ from cride.circles.permissions.memberships import IsActiveCircleMember
 from cride.circles.serializers import MembershipModelSerializer
 
 # Model
-from cride.circles.models import Circle, Membership
+from cride.circles.models import Circle, Membership, Invitation
 
 
 class MembershipViewset(mixins.ListModelMixin,
@@ -54,3 +56,31 @@ class MembershipViewset(mixins.ListModelMixin,
         """Set the Membership relation inactive."""
         instance.is_active = False
         instance.save()
+
+    @action(detail=True, methods=['get'])
+    def invitations(self, request, *args, **kwargs):
+        """Retrieve a member's invitations breakdown.
+
+        Will return a list containing all the members tha have
+        used its invitations and another list containing the
+        invitations tha haven't been used.
+        """
+
+        invited_members = Membership.objects.filter(
+            circle=self.circle,
+            invited_by=request.user,
+            is_active=True
+        )
+
+        unused_invitations = Invitation.objects.filter(
+            circle=self.circle,
+            issued_by=request.user,
+            used=False
+        ).values_list('code', flat=True)
+
+        data = {
+            "used_invitations": MembershipModelSerializer(invited_members, many=True).data,
+            "unsued_invitations": unused_invitations
+        }
+
+        return Response(data, status=status.HTTP_200_OK)
